3feda8ac47cdcfe6232645d98a88c5c38228b3af
sotirpanayotov <sotirpanayotov@yahoo.de>
2015-09-14 23:00:34 +0200

    test commit

--- src/main/java/org/company/panayotov/energy/Main.java
+++ src/main/java/org/company/panayotov/energy/Main.java
@@ -3,5 +3,6 @@ package org.company.panayotov.energy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Our Application starts here");
+        System.out.println("");
     }
 }
